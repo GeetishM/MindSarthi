31e20a9c376fe56ef2c3230610d7d39d89b4e312
GeetishM <geetish1902@gmail.com>
2025-09-19 06:26:22 +0530

    Stop tracking api_service.dart

--- lib/features/personal_user/screens/5chtbotpage/api/api_service.dart
+++
@@ -1,4 +0,0 @@
-class ApiService {
-  static const apiKey = 'AIzaSyBIbrIpz-Pn1aaEPtHFR0PPKM77sesny8g'; // Add your API key here
-  // Its not a good practice to store API key in the code on production
-}
